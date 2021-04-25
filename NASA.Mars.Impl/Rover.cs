7dad69aae97d4bec8a8e9358da3f9c64965276f2
neuralsea-lab <emmett.miller7@outlook.com>
2021-04-25 18:47:33 +0100

    TODO pull up

--- NASA.Mars.Impl/Rover.cs
+++ NASA.Mars.Impl/Rover.cs
@@ -43,23 +43,14 @@ namespace NASA.Mars.Impl
             // TODO pull up to save cycles
             var allowedMovements = new Dictionary<string, (string Orientation, string Command, int X, int Y, string Direction)>();
             // pretty sure there's a bug in here somewhere TODO: complete specs
-            var t1 = (Orientation: "N", Command: "R", X: CurrentPosition.X + command.Units, Y: CurrentPosition.Y, Direction: "E");
-            var t2 = (Orientation: "N", Command: "L", X: CurrentPosition.X - command.Units, Y: CurrentPosition.Y, Direction: "W");          
-            var t3 = (Orientation: "S", Command: "R", X: CurrentPosition.X - command.Units, Y: CurrentPosition.Y, Direction: "W");
-            var t4 = (Orientation: "S", Command: "L", X: CurrentPosition.X + command.Units, Y: CurrentPosition.Y, Direction: "E");
-            var t5 = (Orientation: "E", Command: "R", X: CurrentPosition.X, Y: CurrentPosition.Y - command.Units, Direction: "S");
-            var t6 = (Orientation: "E", Command: "L", X: CurrentPosition.X, Y: CurrentPosition.Y + command.Units, Direction: "N");
-            var t7 = (Orientation: "W", Command: "R", X: CurrentPosition.X, Y: CurrentPosition.Y + command.Units, Direction: "N");
-            var t8 = (Orientation: "W", Command: "L", X: CurrentPosition.X, Y: CurrentPosition.Y - command.Units, Direction: "S");
-
-            allowedMovements.Add("NR", t1);
-            allowedMovements.Add("NL", t2);
-            allowedMovements.Add("SR", t3);
-            allowedMovements.Add("SL", t4);
-            allowedMovements.Add("ER", t5);
-            allowedMovements.Add("EL", t6);
-            allowedMovements.Add("WR", t7);
-            allowedMovements.Add("WL", t8);
+            allowedMovements.Add("NR", (Orientation: "N", Command: "R", X: CurrentPosition.X + command.Units, Y: CurrentPosition.Y, Direction: "E"));
+            allowedMovements.Add("NL", (Orientation: "N", Command: "L", X: CurrentPosition.X - command.Units, Y: CurrentPosition.Y, Direction: "W"));
+            allowedMovements.Add("SR", (Orientation: "S", Command: "R", X: CurrentPosition.X - command.Units, Y: CurrentPosition.Y, Direction: "W"));
+            allowedMovements.Add("SL", (Orientation: "S", Command: "L", X: CurrentPosition.X + command.Units, Y: CurrentPosition.Y, Direction: "E"));
+            allowedMovements.Add("ER", (Orientation: "E", Command: "R", X: CurrentPosition.X, Y: CurrentPosition.Y - command.Units, Direction: "S"));
+            allowedMovements.Add("EL", (Orientation: "E", Command: "L", X: CurrentPosition.X, Y: CurrentPosition.Y + command.Units, Direction: "N"));
+            allowedMovements.Add("WR", (Orientation: "W", Command: "R", X: CurrentPosition.X, Y: CurrentPosition.Y + command.Units, Direction: "N"));
+            allowedMovements.Add("WL", (Orientation: "W", Command: "L", X: CurrentPosition.X, Y: CurrentPosition.Y - command.Units, Direction: "S"));
 
             var tuple = allowedMovements[$"{CurrentPosition.Direction}{command.Direction}"];
             SetPosition(tuple.X, tuple.Y, tuple.Direction);
